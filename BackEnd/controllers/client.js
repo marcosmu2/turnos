83fcc0d44130b5ee96c42781690c08d1e3debae2
Marcos Muñozgit <marcosmunozsistemas@gmail.com>
2022-05-17 15:42:39 -0300

    validaciones en el controller de cliente

--- BackEnd/controllers/client.js
+++ BackEnd/controllers/client.js
@@ -1,54 +1,112 @@
 const {response } =  require('express');
 const Cliente = require('../models/Cliente');
+const { errorReturn } = require('../functions/errorReturnFn');
 
-const createClient= async( req, res = response ) => {
 
-    const cliente = new Cliente(req.body);
+const createClient = async( req, res = response ) => {
 
-    await cliente.save();
+    if (req.body.name === undefined){
+        return res.status(400).json({
+            ok:false,
+            msg:'body debe contener nombre'
+        });
+    }
+    
+    if (req.body.phone === undefined){
+        return res.status(400).json({
+            ok:false,
+            msg:'body debe contener un telefono'
+        });
+    }
 
-    res.status(201).json({
-        ok:true,
-        msg:'registro exitoso'
-    });
+    try {
+
+        const cliente = new Cliente(req.body);
+    
+        await cliente.save();
+    
+        res.status(201).json({
+            ok:true,
+            msg:'registro exitoso'
+        });
+        
+    } catch (error) {
+        errorReturn(res, error);
+
+    }
+
 }
 
 const getAllClient= async( req, res = response ) => {
 
-    let clientes = await Cliente.find().select("_id name phone phone2 address");
+    try {
+               
+        const clientes = await Cliente.find().select("_id name phone phone2 address");
+    
+        res.status(200).json(clientes);
 
-    res.status(200).json(clientes);
+    } catch (error) {
+        errorReturn(res, error);
+    }
+
 }
 
 const deleteClient = async( req, res = response) => {
 
-    
-    await Cliente.findByIdAndDelete(req.query.id)
+    const clienteId = req.query.id;
 
-    res.status(200).json({
-        ok:true,
-        msg:'registro borrado con exito'
-    });
+    try {
+
+        const cliente = await Cliente.findById( clienteId );
+
+        if(cliente != null){
+            await Cliente.findByIdAndDelete(clienteId)
+        
+            res.status(200).json({
+                    ok:true,
+                    msg:'registro borrado con exito'
+                });
+        }else{
+            res.status(400).json({
+                ok:false,
+                msg:'no existe el registro que intenta borrar'
+            });
+        }
+        
+    } catch (error) {
+        errorReturn(res, error);
+    }
+
+    
 }
 
 const updateClient = async(req, res = response) => {
 
     let clienteBody = {...req.body};
-
-    console.log(clienteBody)
+    const clienteId = req.query.id;
 
     try {
-        await Cliente.findByIdAndUpdate({ _id: req.query.id }, clienteBody);
+        const cliente = await Cliente.findById( clienteId );
+
+        if(cliente != null){
+
+            await Cliente.findByIdAndUpdate({ _id: clienteId }, clienteBody);
+            
+            res.status(200).json({
+                ok:true,
+                msg:'registro actualizado con exito'
+            });
+        }else{
+            res.status(400).json({
+                ok:false,
+                msg:'no existe el registro que intenta actualizar'
+            });
+        }
+
     } catch (error) {
-        console.error(error)
+        errorReturn(res, error);
     }
 
-    
-
-    res.status(200).json({
-        ok:true,
-        msg:'registro borrado con exito'
-    });
 }
 
 module.exports = {createClient, getAllClient, deleteClient, updateClient}
